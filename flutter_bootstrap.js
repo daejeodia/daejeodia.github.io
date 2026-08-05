d9b98bd4c104d361b5826e3da4f96f012e20e251
deajeodia <hyung9086@gmail.com>
2026-08-05 11:06:29 +0900

    xxzzz

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2812764531" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3566336967" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
